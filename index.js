d81293ba98104b7a8fe8aac7695eb9562b0f9831
k-kozika <127573443+k-kozika@users.noreply.github.com>
2023-11-16 14:40:18 +0000

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-module.exports = typeof WebAssembly === 'object'
-  ? require('./tesseract-core')
-  : require('./tesseract-core.asm');
+import wasm from "./tesseract-core-simd-lstm.wasm"
+import TesseractCore from "./tesseract-core-simd-lstm.wasm.js"
+
+const TessModule = await TesseractCore({
+  instantiateWasm: (info, receive) => {
+    const instance = new WebAssembly.Instance(wasm, info);
+    receive(instance);
+    return instance.exports;
+  },
+});
+
+export { TessModule }
